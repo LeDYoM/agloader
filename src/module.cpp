b56bb5420831f31113a30fcde20dae26964565e1
Ismael Gonzalez <ledyom@hotmail.com>
2026-07-08 17:04:04 +0200

    Preparing new version

---
+++ src/module.cpp
@@ -0,0 +1,14 @@
+export module agloader:module;
+
+
+namespace agl
+{
+/**
+ * @brief Main class to load a module
+ * This class provides the basic API to load a module and its methods
+ */
+export class Module
+{
+};
+
+}  // namespace agl
