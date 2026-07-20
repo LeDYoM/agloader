4164ff74d2c152c77423c17de08d5852983333b8
Ismael Gonzalez <ledyom@hotmail.com>
2026-07-20 16:58:25 +0200

    Introducing lib for testing

---
+++ tests/main_test_lib_export.hpp
@@ -0,0 +1,15 @@
+#ifndef LOADER_TEST_LIB_EXPORT_INCLUDE_HPP
+#define LOADER_TEST_LIB_EXPORT_INCLUDE_HPP
+
+#ifdef _WIN32
+#define LOADER_API __declspec(dllexport)
+#define LOADER_PRIVATE
+#else
+#define LOADER_API __attribute__((visibility("default")))
+#define LOADER_PRIVATE __attribute__((visibility("hidden")))
+#endif
+#else
+#define LOADER_API
+#define LOADER_PRIVATE
+
+#endif
